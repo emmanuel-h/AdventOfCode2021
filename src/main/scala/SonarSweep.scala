db54a792fe5e435e7bd631e3ec7b2f8dff55c6a3
Emmanuel HERRMANN <emmanuel.herrmann@itsfactory.fr>
2021-12-01 20:03:39 +0100

    Cleanup

--- src/main/scala/SonarSweep.scala
+++ src/main/scala/SonarSweep.scala
@@ -3,16 +3,14 @@ import scala.io.Source
 object SonarSweep {
 
   def main(args: Array[String]): Unit = {
-    val depths = Source.fromResource("day_01_input").getLines()
-      .toList
-      .map(i => i.toInt)
-
+    val depths = readInputs()
     println(computeMeasure(depths, 1))
     println(computeMeasure(depths, 3))
   }
 
-  def computeMeasure(depths: List[Int], measurementSize: Int): Int = {
-    (measurementSize - 1 until depths.size)
-      .count(i => depths.slice(i - measurementSize, i).sum > depths.slice(i - measurementSize - 1, i - 1).sum)
-  }
+  private def computeMeasure(depths: List[Int], measurementSize: Int) =
+    (measurementSize until depths.size).count(i => depths.slice(i - measurementSize, i).sum > depths.slice(i - measurementSize - 1, i - 1).sum)
+
+  private def readInputs() = Source.fromResource("day_01_input").getLines().toList.map(i => i.toInt)
+
 }
