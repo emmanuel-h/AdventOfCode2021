976b83fc94058c47ac438f3e09c8be4958101ffe
Emmanuel HERRMANN <emmanuel.herrmann@itsfactory.fr>
2021-12-01 19:11:25 +0100

    Merge two parts

--- src/main/scala/Day1Part1.scala
+++ src/main/scala/Day1Part1.scala
@@ -3,17 +3,19 @@ import scala.io.Source
 object Day1Part1 {
 
   def main(args: Array[String]): Unit = {
-    var measurementIncreased = 0
-    val list = Source.fromResource("day_01_input").getLines()
+    val depths = Source.fromResource("day_01_input").getLines()
       .toList
       .map(i => i.toInt)
 
-    var previousDepth = list.head
+    println(computeMeasure(depths, 1))
+    println(computeMeasure(depths, 3))
+  }
 
-    list.foreach(actualDepth => {
-      if actualDepth > previousDepth then measurementIncreased = measurementIncreased + 1
-      previousDepth = actualDepth
-    })
-    println(measurementIncreased)
+  def computeMeasure(depths: List[Int], measurementSize: Int): Int = {
+    var measurementIncreased = 0
+    for ((depth, index) <- depths.view.zipWithIndex)
+      if index >= measurementSize && depths.slice(index - measurementSize, index).sum > depths.slice(index - measurementSize - 1, index - 1).sum
+      then measurementIncreased = measurementIncreased + 1
+    measurementIncreased
   }
 }
